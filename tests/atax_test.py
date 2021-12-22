8620a073362a94aeed289177b6bcd99c05e727b0
Jon Kronander <jkr@fastmail.com>
2021-12-22 18:05:09 +0100

    Add support for all tax table numbers (29, 30, etc)

--- tests/atax_test.py
+++ tests/atax_test.py
@@ -9,20 +9,32 @@ def test_out_of_bounds():
         atax.get(-1)
 
 
-@pytest.mark.parametrize('gross_monthly_salary,expected_tax_deduction', [
-    (0, 0),
-    (1, 0),
-    (1600, 0),
-    (1601, 117),
-    (40000, 9314),
-    (40001, 9374),
-    (80000, 29150),
-    (80001, round(0.36 * 80001, 2)), # NOTE: Behavior changes at this salary level
-    (100000, round(0.40 * 100000, 2)),
+@pytest.mark.parametrize('tax_table_number,gross_monthly_salary,expected_tax_deduction', [
+    (29, 0, 0),
+    (29, 1, 0),
+    (29, 1600, 0),
+    (29, 1601, 117),
+    (29, 40000, 9006),
+    (29, 40001, 9064),
+    (29, 80000, 28442),
+    (29, 80001, round(0.36 * 80001)), # NOTE: Behavior changes at this salary level
+    (29, 100000, round(0.39 * 100000)),
+    (29, 200000, round(0.44 * 200000)),
+    (30, 0, 0),
+    (30, 1, 0),
+    (30, 1600, 0),
+    (30, 1601, 117),
+    (30, 40000, 9314),
+    (30, 40001, 9374),
+    (30, 80000, 29150),
+    (30, 80001, round(0.36 * 80001)), # NOTE: Behavior changes at this salary level
+    (30, 100000, round(0.40 * 100000)),
+    (30, 200000, round(0.45 * 200000)),
 ])
-def test_lookup(
+def test_lookup_default_tax_table(
+        tax_table_number,
         gross_monthly_salary,
         expected_tax_deduction
     ):
-    atax = ATax()
+    atax = ATax(tax_table_number)
     assert atax.get(gross_monthly_salary) == expected_tax_deduction
